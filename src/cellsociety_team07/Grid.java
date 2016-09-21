1b812398c6ecd1b1a470762385780eb2300fc47d
Daniel Chai <dhc10@duke.edu>
2016-09-21 12:55:28 -0400

    Implement basic functionality of Grid, Cell, and Neighborhood

--- src/cellsociety_team07/Grid.java
+++ src/cellsociety_team07/Grid.java
@@ -4,5 +4,31 @@ package cellsociety_team07;
  * This class represents a grid of Cells.
  */
 public class Grid {
-
+	private Cell[][] grid;
+	
+	/**
+	 * Constructor for Grid class
+	 * @param rows number of rows in the grid
+	 * @param columns number of columns in the grid
+	 */
+	public Grid(int rows, int columns) {
+		grid = new Cell[rows][columns];
+	}
+	
+	/**
+	 * @return the 2-D array of Cells representing the Grid
+	 */
+	public Cell[][] getGrid() {
+		return grid;
+	}
+	
+	/**
+	 * Adds a Cell to the grid
+	 * @param cell the Cell to add
+	 * @param row the row to place the Cell in
+	 * @param col the column to place the Cell in
+	 */
+	public void addCell(Cell cell, int row, int col) {
+		grid[row][col] = cell;
+	}
 }
--- src/cellsociety_team07/State.java
+++ src/cellsociety_team07/State.java
@@ -4,5 +4,12 @@ package cellsociety_team07;
  * This class represents the state that a Cell is in.
  */
 public class State {
-
+	
+	
+	/**
+	 * Constructor for State class.
+	 */
+	public State() {
+		
+	}
 }
