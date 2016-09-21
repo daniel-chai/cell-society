42364f5b1ed8436775b9766484951d506c502971
Daniel Chai <dhc10@duke.edu>
2016-09-20 20:49:57 -0400

    Create UIGenerator to provide methods that can generator common UI components such as Text, Button, Rectangle

---
+++ src/cellsociety_team07/UIGenerator.java
@@ -0,0 +1,98 @@
+package cellsociety_team07;
+
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+/**
+ * This class generates common JavaFX UI components based on the input parameters.
+ * The supported UI components are Text, Button, and Rectangle. All the methods are
+ * static because they simply take in parameters and return a new UI component. This
+ * class doesn't have instance variables and there is no sensible meaning of instantiating
+ * a UIGenerator object, so it makes more sense to have static methods. 
+ */
+public class UIGenerator {
+	public static final String DEFAULT_FONT_FAMILY = "Verdana";
+	public static final int DEFAULT_FONT_SIZE = 20;
+	public static final double DEFAULT_BUTTON_WIDTH = 100;
+	public static final double DEFAULT_BUTTON_HEIGHT = 25;
+	
+	/**
+	 * @param text String to fill the text
+	 * @param x x-coordinate of the text
+	 * @param y y-coordinate of the text
+	 * @return Text with the specified attributes
+	 */
+	public static Text createText(String text, double x, double y) {
+		return createText(text, x, y, DEFAULT_FONT_SIZE);
+	}
+	
+	/**
+	 * @param text String to fill the text
+	 * @param x x-coordinate of the text
+	 * @param y y-coordinate of the text
+	 * @param fontSize font size of the text
+	 * @return Text with the specified attributes
+	 */
+	public static Text createText(String text, double x, double y, int fontSize) {
+		Text textUI = new Text();
+		textUI.setText(text);
+		textUI.setX(x);
+		textUI.setY(y);
+		textUI.setFont(Font.font(DEFAULT_FONT_FAMILY, fontSize));
+		
+		return textUI;
+	}
+	
+	/**
+	 * @param text String to fill the button
+	 * @param x x-coordinate of the button
+	 * @param y y-coordinate of the button
+	 * @return Button with the specified attributes
+	 */
+	public static Button createButton(String text, double x, double y) {
+		return createButton(text, x, y, DEFAULT_BUTTON_WIDTH, DEFAULT_BUTTON_HEIGHT, DEFAULT_FONT_SIZE);
+	}
+	
+	/**
+	 * @param text String to fill the button
+	 * @param x x-coordinate of the button
+	 * @param y y-coordinate of the button
+	 * @param width width of the button
+	 * @param height height of the button
+	 * @param fontSize font size of the button
+	 * @return Button with the specified attributes
+	 */
+	public static Button createButton(String text, double x, double y, double width, double height, int fontSize) {
+		Button buttonUI = new Button();
+		buttonUI.setText(text);
+		buttonUI.setLayoutX(x);
+		buttonUI.setLayoutY(y);
+		buttonUI.setMinWidth(width);
+		buttonUI.setMinHeight(height);
+		buttonUI.setFont(Font.font(DEFAULT_FONT_FAMILY, fontSize));
+		
+		return buttonUI;
+	}
+	
+	/**
+	 * @param color color of the rectangle
+	 * @param x x-coordinate of the rectangle
+	 * @param y y-coordinate of the rectangle
+	 * @param width width of the rectangle
+	 * @param height height of the rectangle
+	 * @return Rectangle with the specified attributes
+	 */
+	public static Rectangle createRectangle(Color color, double x, double y, double width, double height) {
+		Rectangle rectangleUI = new Rectangle();
+		rectangleUI.setFill(color);
+		rectangleUI.setX(x);
+		rectangleUI.setY(y);
+		rectangleUI.setWidth(width);
+		rectangleUI.setHeight(height);
+		
+		return rectangleUI;
+	}
+}
